b913b924d99cb29a3e9311020c9942787bd85a10
Li-Hsuan Huang <amoslihuang@gmail.com>
2018-03-12 14:29:12 -0700

    Update expected_visits.py

--- expected_visits.py
+++ expected_visits.py
@@ -1,4 +1,4 @@
-# Example of computing expected visits before absorption.
+# Example of computing expected visits before absorption
 # Absorbing state is arranged so it is on the first row 
 # of the probability transition matrix.
 
@@ -17,9 +17,9 @@ P = np.concatenate((row1,row2,row3,row4,row5),axis=0)
 # Obtain substochatstic matrix Q.
 Q = P[1:,1:]
 
-# Obtain the matrix of expected visits from one state to another bofore absorption
+# Obtain the matrix of expected visits from one state to another bofore absorption.
 M = np.linalg.inv(np.eye(Q.shape[0]) - Q)
 
-# The expected number of visits to get from state i to absoprtion
+# Compute expected number of visits to get from state i to absoprtion.
 # This is the same as doing np.dot(M,np.ones(Q.shape[0])).
 E = np.sum(M,1)
